a1ee065c291379a73e1fd0af95d33da1c87ed92b
bolei <bolei@cmu.edu>
2013-03-25 12:09:43 -0400

    added simple tokenizer for tokenizing raw corpus

---
+++ tools/clean-dict.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import fileinput
+import re
+
+_digits = re.compile('\d')
+def contains_digits(d):
+    return bool(_digits.search(d))
+
+def main():
+	wordToAppend="AjectivesInf"
+	for line in fileinput.input('!'):
+		word = line.strip()
+		if word != "" and len(word) > 1 and len(word.split()) == 1 and (word.isalpha() or word.find('.') == -1) and contains_digits(word) == False:
+			print word + "\t"+wordToAppend+";"
+
+if __name__ == "__main__":
+
+	main()
+#	str = u"Æ"
+#	print type(str)
+#	print len(str)
---
+++ tools/simple-tokenizer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import fileinput
+
+ignorelist = ('!', '-', '_', '(', ')', ',', '.', ':', ';', '"', '\'', '?')
+
+def displayword(w):
+	if w != "":
+		print w
+
+def handletail(w):
+#	print w
+	if(w == ""):
+		return
+	if(ignorelist.count(w[len(w)-1]) > 0):
+		handletail(w[0:len(w)-1])
+		displayword(w[len(w)-1])
+	else:
+		displayword(w[0:len(w)])
+
+def handleword(w):
+	pos = w.find('\'')
+	if(pos >= 0):
+		displayword(w[0:pos+1])
+		w = w[pos+1:len(w)]
+	handletail(w)
+
+def main():
+	for line in fileinput.input():
+		words = line.split()
+		for w in words:
+			handleword(w)
+
+if __name__ == "__main__":
+	main()
+#	handletail('abc...')
